81f0dce9fbc3f83131ecd12a2053ea678fec5cca
leewater <leewatertrue@gmail.com>
2025-04-02 20:49:16 +0900

    [0402] 이수진

---
+++ phone/phone_prj/phone/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Phone
+
+def list(request):
+    phones=Phone.objects.all().order_by('name')
+    return render(request, 'phone/list.html',{'phones':phones})
+
+def result(request):
+    keyword = request.GET.get('keyword') 
+    results = Phone.objects.filter(name__contains=keyword).order_by('name')
+    context={
+        'keyword': keyword,
+        'results': results,
+    }
+    return render(request, 'phone/result.html', context)
